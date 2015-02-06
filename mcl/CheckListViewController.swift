87db04acf0daac8befb03a1f28502c94d0052acb
yan xia <yanxia@yans-mbp.corp.kabam.com>
2015-02-05 17:52:53 -0800

    ongoing

---
+++ mcl/CheckListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  mcl
+//
+//  Created by yan xia on 2/5/15.
+//  Copyright (c) 2015 yan xia. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as UITableviewcell
+        return cell
+    }
+    
+    
+    
+
+
+}
+
